9ad8675666550c4e4d1c4638c1650682e9303ef0
Hossam Nofal <hnofal@bass.com.eg>
2018-07-26 04:03:28 +0200

    V2

---
+++ WebAPIMovies/Controllers/Comment_MovieController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebAPIMovies.DB;
+
+namespace MovieWebApi.Controllers
+{
+    
+    public class Comment_MovieController : ApiController
+    {
+        
+        MoviesDBDataContext MOVDB = new MoviesDBDataContext();
+
+   // [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]  
+        public IEnumerable<Get_Movie_CommentResult> GetWishMovie(int UserID)
+        {
+            var Commentslist = MOVDB.Get_Movie_Comment(UserID).ToList();
+            return Commentslist;
+            
+        }
+
+
+     //   [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]  
+        // POST: api/Movies
+        [HttpPost]
+        public int PostInsertMovieComment(int ID_User, string Comment_movie,  int Movie_id,int Rating)
+        {
+
+            return MOVDB.Insert_wish_movies_list(ID_User,Comment_movie,Movie_id,Rating);
+        }
+    }
+}
---
+++ WebAPIMovies/Controllers/LoginController.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIMovies.DB;
+
+namespace MovieWebApi.Controllers
+{
+    public class LoginController : ApiController
+    {
+        MoviesDBDataContext MOVDB = new MoviesDBDataContext();
+        // GET: api/Login
+        [HttpGet]
+        public Login_userResult GetLoginUser(string UserName, string Passowrd)
+        {
+           
+           var userdata =  MOVDB.Login_user(Passowrd, UserName).SingleOrDefault();
+            
+            
+            return userdata;
+        }
+
+        [HttpPost]
+        [Route("Regstration")]
+        // GET: api/regest
+        public IEnumerable<insert_New_UserResult> PostRegstration(string email, string Passowrd, string UserName, string First_Name, string lastname, string soicalid, string oauthtype)
+        {
+            
+            var Isregst = MOVDB.insert_New_User(email, Passowrd, UserName,First_Name,lastname,soicalid,oauthtype);
+           
+            return Isregst;
+        }
+
+        [HttpPost]
+        [Route("Update/UserInfo")]
+        // GET: api/regest
+        public int PostUpdateuser(int userid,string email, string Passowrd, string UserName, string First_Name, string lastname)
+        {
+
+            var Isregst = MOVDB.Update_user(userid, UserName, email, Passowrd, First_Name, lastname);
+
+            return Isregst;
+        }
+        
+        [Route("getUser")]
+        // GET: api/regest
+        public IEnumerable<getuserResult> GetUser(string soicalid)
+        {
+
+            var Isregst = MOVDB.getuser( soicalid).ToList();
+
+            return Isregst;
+        }
+
+        
+    }
+}
